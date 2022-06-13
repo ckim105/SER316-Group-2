3abe18b14bd32acdb0c889bcc5a615a5505cfe6d
knguye71 <knguye71@asu.edu>
2022-06-13 11:46:18 -0700

    Added in test case classes

---
+++ src/test/Sprint2_Blackbox.java
@@ -0,0 +1,11 @@
+package test;
+
+import main.java.memoranda.ui.TaskDialog;
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Sprint2_Blackbox {
+}
