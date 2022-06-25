c6d9581bf30eb5ffca5954e7aac765497c643fae
knguye71 <knguye71@asu.edu>
2022-06-24 21:55:11 -0700

    US#64 Test Cases

--- src/test/Sprint3_Whitebox.java
+++ src/test/Sprint3_Whitebox.java
@@ -1,6 +1,8 @@
 package test;
 
 import main.java.memoranda.ui.TaskDialog;
+import main.java.memoranda.ui.Team;
+
 import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
@@ -8,4 +10,21 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Sprint3_Whitebox {
+	//===================================US#64 Tests================================================
+		/**
+		 * Tests for User Story #64. Test if Team getters work.
+		 */
+		  /**
+		   * Test 1: Test that creating a team works.
+		   * @since 1.0
+		   */
+		  @Test
+		  public void teamWorks() {
+		      
+		      Team test1 = new Team("Alpha", "a", 1, "a", 1, "a", 1, "a", 1);
+		      
+		      String comp = "Alpha";
+		      
+		      assertTrue(comp == test1.getTeamName());
+		}	
 }
