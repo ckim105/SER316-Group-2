3f51632577d8f0d2c195e50dbc893d1286652fb5
knguye71 <knguye71@asu.edu>
2022-06-24 21:48:23 -0700

    US#64 Checkstyle removed 23 errors

--- src/main/java/memoranda/ui/Team.java
+++ src/main/java/memoranda/ui/Team.java
@@ -8,10 +8,10 @@
 package main.java.memoranda.ui;
 
 public class Team {
-	private Member teamM1, teamM2, teamM3, teamM4;
-	private String teamName;
-	
-	/**
+    private Member teamM1, teamM2, teamM3, teamM4;
+    private String teamName;
+
+     /**
 	 * Constructor for Team
 	 * @param inTeamName
 	 * @param inTeamM1
@@ -23,49 +23,49 @@ public class Team {
 	 * @param inTeamM4
 	 * @param inTM4R
 	 */
-	public Team(String inTeamName, String inTeamM1, int inTM1R, String inTeamM2, int inTM2R,
+    public Team(String inTeamName, String inTeamM1, int inTM1R, String inTeamM2, int inTM2R,
 			String inTeamM3, int inTM3R, String inTeamM4, int inTM4R) {
 		teamName = inTeamName;
 		teamM1 = new Member(inTeamM1, inTM1R);
 		teamM2 = new Member(inTeamM2, inTM2R);
 		teamM3 = new Member(inTeamM3, inTM3R);
 		teamM4 = new Member(inTeamM4, inTM4R);
-	}
+    }
 	
 
     public Member getMember1() {
-		return teamM1;	
-	}
+	    return teamM1;	
+    }
     
     public Member getMember2() {
-		return teamM2;	
-	}
+	    return teamM2;	
+    }
     
     public Member getMember3() {
-		return teamM3;	
-	}
+	    return teamM3;	
+    }
     
     public Member getMember4() {
-		return teamM4;	
-	}
+	    return teamM4;	
+    }
     
     public String getTeamName() {
-    	return teamName;
+        return teamName;
     }
     
     public String getName(Member m) {
-		return m.getName();	
-	}
+	    return m.getName();	
+    }
     
     public String getRole(Member m) {
-		return m.getRole();	
-	}
+	    return m.getRole();	
+    }
 
 	
 	/**
 	 * Team Member Object
 	 */
-	private static class Member {
+    private static class Member {
 		private String username;
 		private enum Role {SCRUMMASTER, GITMASTER, CODER};
 		private Role role;
